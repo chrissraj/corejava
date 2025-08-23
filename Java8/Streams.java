a99dadfe6d7ab4b01e196bb4eb1e347898a78d75
chrissraj <christiansugantharaj@gmail.com>
2025-08-23 13:39:29 +0530

    Filter for even numbers in streams

--- Java8/Streams.java
+++ Java8/Streams.java
@@ -7,6 +7,8 @@ public class Streams {
     public static void main(String[] args) {
         List<Integer> numbers = List.of(7,6,5,4,3,2,1);
         System.out.println(numbers.stream().sorted().toList());
+
+        System.out.println(numbers.stream().filter(x->x%2==0).toList());
     }
     
 }
