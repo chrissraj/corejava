eb3db0b8f3871a25f4999b79fb7a079f3a0b9d9c
chrissraj <christiansugantharaj@gmail.com>
2025-08-24 16:09:29 +0530

    Added anymethod method to Stream

--- Java8/Streams.java
+++ Java8/Streams.java
@@ -11,6 +11,8 @@ public class Streams {
         System.out.println(numbers.stream().filter(x->x%2==0).toList());
 
         System.out.println(numbers.stream().map(t->t*2).toList());
+
+        System.out.println(numbers.stream().anyMatch(x->x%2==1));
     }
     
 }
