7b5433dd9b265afec51bb3dee97e56c280563c4e
chrissraj <christiansugantharaj@gmail.com>
2025-08-24 16:01:36 +0530

    new inheritance class added

---
+++ Inhertiance.java
@@ -0,0 +1,24 @@
+package Java;
+
+public class Inhertiance {
+    
+    public static void main(String[] args) {
+        Car car = new Mercedez();
+        car.name();
+    }
+}
+
+
+class Car{
+    void name(){
+        System.out.println("This is car");
+    }
+}
+
+class Mercedez extends Car{
+
+    @Override
+    void name(){
+        System.out.println("This is Mercedes");
+    }
+}
