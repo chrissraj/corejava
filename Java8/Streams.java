b391d10dbe4a3ea243859030fb5ae053503a8623
chrissraj <christiansugantharaj@gmail.com>
2025-08-25 08:02:50 +0530

    Changes in stream file - new methods added

--- Java8/Streams.java
+++ Java8/Streams.java
@@ -13,7 +13,9 @@ public class Streams {
         System.out.println(numbers.stream().map(t->t*2).toList());
 
         System.out.println(numbers.stream().anyMatch(x->x%2==1));
-        System.out.println(numbers.stream().allMatch(x->x%2==0));
+        System.out.println(numbers.stream().toList());
+
+        System.out.println(numbers.stream().findAny());
     }
     
 }
