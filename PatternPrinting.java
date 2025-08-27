94224fa3897dbca07299014a16e70a8cf0887cf3
justacder <christiansugantharaj28@gmail.com>
2025-08-27 20:09:34 +0530

    Update PatternPrinting.javaff

--- PatternPrinting.java
+++ PatternPrinting.java
@@ -10,7 +10,7 @@ public class PatternPrinting {
         }
        }
 
-       System.out.println("Pattern printing is nice, Pattern printing helps build logic");
+       System.out.println("Pattern printing is nice");
 
        for(int j=0;j<10;j++){
         System.out.println("great");
