4d27192a24cab3a12c3435d81226f6541515c52b
chrissraj <christiansugantharaj@gmail.com>
2025-08-27 20:02:29 +0530

    Update PatternPrinting.java files

--- PatternPrinting.java
+++ PatternPrinting.java
@@ -4,13 +4,13 @@ public class PatternPrinting {
     
     public static void main(String[] args) {
         
-       for(int i=0;i<5;i++){
-        for(int j=0;j<5;j++){
+       for(int i=0;i<100;i++){
+        for(int j=0;j<50;j++){
             System.out.println("@");
         }
        }
 
-       System.out.println("Pattern printing is cool");
+       System.out.println("Pattern printing is cool, Pattern printing is nice");
 
        for(int i=0;i<5;i++){
         for(int j=0;j<i;j++){
