25fa5e155bfa891af16298fa6ca4b99312c00a7f
justacder <christiansugantharaj28@gmail.com>
2025-08-27 23:09:29 +0530

    made changes in pattern printing class in main

--- PatternPrinting.java
+++ PatternPrinting.java
@@ -3,18 +3,18 @@ package Java;
 public class PatternPrinting {
     
     public static void main(String[] args) {
-        
-       for(int i=0;i<100;i++){
-        for(int j=0;j<50;j++){
-            System.out.println("@");
+    System.out.println("This is a pattern printing");
+       for(int i=0;i<9;i++){
+        for(int j=0;j<9;j++){
+            System.out.println("#");
         }
        }
 
        System.out.println("Pattern printing is cool, Pattern printing is nice");
 
-       for(int i=0;i<5;i++){
+       for(int i=0;i<9;i++){
         for(int j=0;j<i;j++){
-            System.out.println("@");
+            System.out.println("#");
         }
        }
     }
