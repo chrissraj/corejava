0da36aa6a4a984e21cec2e4dff35ce0a9444d6e3
justacder <christiansugantharaj28@gmail.com>
2025-08-27 19:40:57 +0530

    Update PatternPrinting.java

--- PatternPrinting.java
+++ PatternPrinting.java
@@ -6,12 +6,16 @@ public class PatternPrinting {
         
        for(int i=0;i<5;i++){
         for(int j=0;j<5;j++){
-            System.out.println("$");
+            System.out.println("@");
         }
        }
 
        System.out.println("Pattern printing is cool");
 
-       
+       for(int i=0;i<5;i++){
+        for(int j=0;j<i;j++){
+            System.out.println("@");
+        }
+       }
     }
 }
